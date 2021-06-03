d959432de7b591e2e7740bcaf7ef939eee7d99e4
Keisuke Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2021-06-03 13:51:13 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo apt install wiringPi
+gcc -o boot_LED.out boot_LED.c -lwiringPi
+gcc -o main.out display.c -lwiringPi
+gcc -o scheduled_reboot.out scheduled_reboot.c -lwiringPi
+gcc -o shutdown.out shutdown.c -lwiringPi
+sudo mkdir /opt/LCD_Programs
+sudo mv ./*.out /opt/LCD_Programs
+sudo cp ./*.service /etc/systemd/system/
+sudo cp ./*.timer /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable LCD_Programs.service
+sudo systemctl active LCD_Ptograms.service
+sudo systemctl enable scheduled_reboot.timer
+echo ""
+echo "Please Reboot to start LCD Programs."
